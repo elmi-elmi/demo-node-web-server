6a1b63a0203e387c2f8ff93991baa135b4ff9fdf
elmi-elmi <shahrokhelmi@gmail.com>
2022-05-27 14:41:51 +0430

    404

--- src/app.js
+++ src/app.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('hbs');
 
-const publicDirectoryPath = path.join(__dirname,'../public')
-
+const publicDirectoryPath = path.join(__dirname,'../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname,'../templates/partials');
 const app = express();
 
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
 app.use(express.static(publicDirectoryPath));
 
+hbs.registerPartials(partialsPath);
 
 app.get('',(req,res)=>{
-    res.render('index')
+    res.render('index',{
+        title:'Weather'
+    })
 })
 app.get('/about',(req,res)=>{
-    res.render('about')
+    res.render('about',{
+        title:'About'
+    })
 })
 app.get('/help',(req,res)=>{
     res.render('help',{
@@ -26,6 +34,17 @@ app.get('/weather',(req,res)=>{
         forecast:'sunny',
         location:'shiraz'
     });
+})
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        errorMessage: 'this articl in help section not found',
+
+    })
+})
+app.get('*',(req,res)=>{
+    res.render('404',{
+        errorMessage: 'This page not found (404)',
+    })
 })
 
 app.listen(3000,()=>{
