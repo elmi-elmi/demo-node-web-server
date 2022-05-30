574d5ab6404557bc981543d73c449464c7c3c370
elmi-elmi <shahrokhelmi@gmail.com>
2022-05-30 21:17:29 +0430

    creating search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,33 @@
 console.log('Client side javascript file is loaded.')
 
+//
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
+//
+//
+// const url = 'http://localhost:3000/weather?address=shiraz';
+//
+// fetch(url).then((response)=>{
+//     response.json().then((data)=>{
+//         if(data.error) return console.log(data.error);
+//         console.log(data)
+//     })
+// })
+const baseurl = 'http://localhost:3000/weather?address='
+const form = document.querySelector('form');
+const search = document.querySelector('input');
 
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const location = search.value;
+    fetch(baseurl+location).then((response)=>{
+        response.json().then((data)=>{
+            if(data.error) return console.log(data.error);
+            console.log(data)
+        })
     })
-})
 
-
-const url = 'http://localhost:3000/weather?address=shiraz';
-
-fetch(url).then((response)=>{
-    response.json().then((data)=>{
-        if(data.error) return console.log(data.error);
-        console.log(data)
-    })
 })
